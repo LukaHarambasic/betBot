001831b715f5e80f782299fa58092925a8fd5a50
LukaHarambasic <luka@harambasic.de>
2021-06-17 13:03:42 +0200

    changes logic, less goals

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import {ElementHandle, chromium, Page} from 'playwright'
 import {
     env,
-    flipACoin,
-    getPreferredGoal,
-    getRandomGoal,
     isPreferredTeam,
-    isPreferredTeamInTeams, isTieRealistic,
-    randomNumberInInterval
+    isPreferredTeamInTeams, isTieRealistic, probability,
 } from './helper'
 import { config } from 'dotenv'
 
@@ -26,18 +22,24 @@ const getTeams = async (match: ElementHandle): Promise<string[]> => {
 
 const getOdds = async (match: ElementHandle): Promise<number[]> => {
     const odds = await match.$$('.kicktipp-wettquote')
-    return await Promise.all(odds.map(async (odd) => Number(await odd.innerText())))
+    return await Promise.all(odds.map(async (odd) => {
+        const oddValue = Number(await odd.innerText())
+        return Math.sqrt(oddValue)
+    }))
 }
 
 const getGoals = async (match: ElementHandle): Promise<number[]> => {
     const teams = await getTeams(match)
+    console.log('teams', teams)
     const odds = await getOdds(match)
-    // TODO: if text "Spieltag" is in ".prevnextTitle > a" a tie is possible
+    if(odds.length === 0) {
+        return []
+    }
+    // TODO: depends on the rules if ties are possible in the ko matches
     const isTiePossible = true
-    // removed  && flipACoin from the condition
     if(isTieRealistic(odds) && isTiePossible && !isPreferredTeamInTeams(teams)) {
         // If a tie is possible, realistic and the randomizer wants one
-        const goal = randomNumberInInterval(0, Number(env('TIE_MAX')))
+        const goal = Math.round(Math.sqrt(odds[1]))
         return [goal, goal]
     } else {
         return [
@@ -47,23 +49,10 @@ const getGoals = async (match: ElementHandle): Promise<number[]> => {
     }
 }
 
-// TODO rename - better idea than weighted?
-// TODO more granular?
 const getWeightedGoal = (odd: number, teams: string[], currentTeam: string): number => {
-    const goal = getPreferredGoal(isPreferredTeam(currentTeam), getRandomGoal(odd))
-    // to many goals per match with high odds so I do some "magic"
-    if(goal >= 5) {
-        if(flipACoin()) {
-            return Math.ceil(Math.sqrt(goal)) + 1
-        }
-        return Math.ceil(Math.sqrt(goal))
-    }
-    // if goal is > 1 subtract 1 depending on coin flip, otherwise 0 goals aren't possible
-    if(goal >= 1 && flipACoin()) {
-        return goal - 1
-    }
-    // increase the chance of 0 goals
-    if(goal === 1 && flipACoin()) {
+    const bonus = Number(env('FAVORITE_BONUS'))
+    const goal = isPreferredTeam(currentTeam) ? (Math.round(odd) + bonus) : Math.round(odd)
+    if(goal >= 1 && probability(0.2)) {
         return goal - 1
     }
     return goal
@@ -81,23 +70,30 @@ const login = async (page: Page) => {
 }
 
 const bet = async (page: Page) => {
-    // await page.waitForSelector('text="Tippabgabe"')
     const groupPhaseElement = await page.$$('.prevnextTitle')
-    // TODO get text and check if "Spieltag"
     const matches = await page.$$('#tippabgabeSpiele tbody .datarow')
+    let hasTobeSubmitted = false
     for await (const match of matches) {
         const [input1, input2] = await match.$$('input[inputmode="numeric"]')
         const goals = await getGoals(match)
-        await input1.fill(String(goals[0]))
-        await input2.fill(String(goals[1]))
+        const isFinished = input1 === undefined && input2 === undefined // you cant bet on finished matches - finished = no input field
+        const hasOdds = goals.length > 0 // you cant bet on matches where there arent odds - no goals = no odds
+        console.log('goals', goals)
+        if (!isFinished && hasOdds) {
+            await input1.fill(String(goals[0]))
+            await input2.fill(String(goals[1]))
+            hasTobeSubmitted = true
+        }
     }
-    await page.click('input[name="submitbutton"]')
+    if(hasTobeSubmitted) {
+        await page.click('input[name="submitbutton"]')
+    }
 }
 
 const betPerMatchDay = async (page: Page) => {
     const url = `https://www.kicktipp.de/${TEAM}/tippabgabe?&spieltagIndex=`
     // TODO extend for finals
-    const matchDays = [1,2,3,4,5,6]
+    const matchDays = [1,2,3,4,5,6,7,8,9,10]
     for await (const matchDay of matchDays) {
         await page.goto(url + matchDay)
         await bet(page)
@@ -105,7 +101,8 @@ const betPerMatchDay = async (page: Page) => {
 }
 
 (async () => {
-    const browser = await chromium.launch({ headless: false, slowMo: Number(env('SLO_MO')) })
+    const isHeadless = Boolean(JSON.parse(env('HEADLESS')))
+    const browser = await chromium.launch({ headless: isHeadless, slowMo: Number(env('SLO_MO')) })
     const context = await browser.newContext({
         locale: env('LANGUAGE'),
     })
@@ -114,10 +111,13 @@ const betPerMatchDay = async (page: Page) => {
         const url = `https://www.kicktipp.de/${TEAM}/tippabgabe`
         await page.goto(url)
         await login(page)
-        // await page.click('#accept-choices')
+        const acceptCookies = await page.$$('#accept-choices')
+        if(acceptCookies) {
+            await page.click('#accept-choices')
+        }
         await betPerMatchDay(page)
         // await browser.close()
-        console.log('Browser closed')
+        // console.log('Browser closed')
     } catch (error) {
         console.log('ERROR: ', error)
         await browser.close()
